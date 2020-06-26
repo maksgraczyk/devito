c92874d4df58f93e042a3d2ec4d6003a3cfadfee
Maksymilian Graczyk <maksymilian.graczyk18@imperial.ac.uk>
2020-06-26 18:57:46 +0200

    Add padding to B necessary for R to be complete in sobel.py

--- examples/ml/sobel.py
+++ examples/ml/sobel.py
@@ -1,22 +1,22 @@
-from devito import Function, dimensions, Operator, Eq
+from devito import Function, dimensions, Operator, Eq, Grid
 
 # Expected result (R.data):
 # [[ 34. 32. -16. -22. -18.]
-# [ 22. 30.   6.  -9. -28.]
-# [ 21. 12.  12.   4. -33.]
-# [ 37.  7.   7.  -3. -44.]
-# [ 25. 10.  10. -10.  35.]]
+#  [ 22. 30.   6.  -9. -28.]
+#  [ 21. 12.  12.   4. -33.]
+#  [ 37.  7.   7.  -3. -44.]
+#  [ 25. 10.  10. -10.  35.]]
 
 
 def run():
-    m, n, k, l = dimensions('m n k l')
-    A = Function(name='A', shape=(3, 3), dimensions=(m, n))
-    B = Function(name='B', shape=(5, 5), dimensions=(k, l))
-    R = Function(name='R', shape=(5, 5), dimensions=(k, l))
-    op = Operator(Eq(R,
-                     sum([A[2 - i, 2 - j]
-                          * B[k + i - 1, l + j - 1] for i in range(3)
-                          for j in range(3)])))
+    m, n = dimensions('m n')
+    gridA = Grid(shape=(3, 3), dimensions=(m, n))
+    gridBR = Grid(shape=(5, 5))
+
+    A = Function(name='A', grid=gridA, space_order=0)
+    B = Function(name='B', grid=gridBR, space_order=1)
+    R = Function(name='R', grid=gridBR, space_order=0)
+
     A.data[:] = [[1, 0, -1],
                  [2, 0, -2],
                  [1, 0, -1]]
@@ -25,8 +25,13 @@ def run():
                  [1, 2, 3, 4, 5],
                  [15, 15, 15, 15, 15],
                  [5, 5, 10, 10, 5]]
-    print(op.ccode)
-    print(B.data_with_halo)
+
+    x, y = B.dimensions
+    op = Operator(Eq(R,
+                     sum([A[2 - i, 2 - j]
+                          * B[x + i - 1, y + j - 1] for i in range(3)
+                          for j in range(3)])))
+
     op.apply()
     print(R.data)
 
